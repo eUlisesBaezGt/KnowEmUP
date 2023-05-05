94c32c1e33ff75245eef737b36a4c88985cf94f6
eUlisesBaezGt <89673617+eUlisesBaezGt@users.noreply.github.com>
2023-05-05 08:07:52 -0600

    AVANCE REACT

---
+++ dashboard/src/data/mockData.js
@@ -0,0 +1,18 @@
+export const mockDataTeam = [
+    {
+        id: 1,
+        name: "Jon Snow",
+        email: "jonsnow@gmail.com",
+        age: 35,
+        phone: "(665)121-5454",
+        access: "admin"
+    },
+    {
+        id:2,
+        name: "Cersei Lannister",
+        email: "cerseilannister@gmail.com",
+        age: 42,
+        phone: "(421)314-2288",
+        access: "manager"
+    }
+]
