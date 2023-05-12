d0933b3fe327578b74b07edf37a59cc53d6d7861
eUlisesBaezGt <89673617+eUlisesBaezGt@users.noreply.github.com>
2023-05-12 11:49:46 -0600

    DB

--- DataBase/SQLs/tests_for_db.sql
+++ DataBase/SQLs/tests_for_db.sql
@@ -1,58 +1,60 @@
+# ESTABLECER BASE DE DATOS PARA USAR
 USE KnowEmUP;
-SELECT COUNT(*) FROM teachers; # 23 teachers
+# CUANTOS PROFESORES HAY
+SELECT COUNT(*) FROM teachers; # 24 teachers
 
 SELECT COUNT(*) FROM users; # 10 users
 
-SELECT COUNT(*) FROM teacher_grades; # 3000 graded
-
-SELECT COUNT(*) FROM teacher_grades; # 220 grades
+SELECT COUNT(*) FROM teacher_grades; # 6000 grades
 
 SELECT * FROM teacher_grades
-WHERE teacherID = 'David Tovar'; # 10 grades SAME AS BELOW
+WHERE teacherID = 'David Tovar'; # 256 grades SAME AS BELOW
 
 SELECT COUNT(*) FROM teacher_grades
-WHERE teacherID = 'David Tovar'; # 10 grades
+WHERE teacherID = 'David Tovar'; # 256 grades
 
+# Las grades de profesor, se usan para sacar las estrellas
 SELECT AVG(grade_profesor) FROM teacher_grades
-WHERE teacherID = 'David Tovar'; # 75 TODO VERIFICAR A MANO (81 con el cambio)
+WHERE teacherID = 'David Tovar'; # Los alumnos le dan 81
 
+# Las grades del alumno, es cuanto sacas al cursar su materia
 SELECT AVG(grade_alumno) FROM teacher_grades
-WHERE teacherID = 'David Tovar'; # 63 TODO VERIFICAR A MANO (78 con el cambio)
+WHERE teacherID = 'David Tovar'; # Los alumnos sacan 78
 
+# Te filtra el promedio de las califs del profesor cuando es mayor a 80
 SELECT COUNT(grade_profesor) FROM teacher_grades
 WHERE teacherID = 'David Tovar' AND grade_profesor > 80;
 
 SELECT * FROM teacher_grades
 WHERE teacherID = 'David Tovar' AND grade_profesor > 80;
 
-SELECT COUNT(*) FROM teacher_grades
-WHERE grade_alumno = 100; # 5 (78 con el cambio)
+# Conteo de alumnos que han obtenido 100 en alguna materia
+SELECT COUNT(DISTINCT studentID) FROM teacher_grades
+WHERE grade_alumno = 100; # 156 grades // PERO solo 10 alumnos
 
 SELECT * FROM teacher_grades
-WHERE grade_alumno = 100; # CONFIRMA QUE SON 5
+WHERE grade_alumno = 100;
 
+# Filtrar el ID de los alumnos que han obtenido 100 en alguna materia
 SELECT studentID FROM teacher_grades
-WHERE grade_alumno = 100; # 5
+WHERE grade_alumno = 100;
 
+# Filtrar el ID de los profesores que han obtenido 100 en alguna materia
 SELECT DISTINCT teacherID FROM teacher_grades
 WHERE grade_profesor = 100;
 
-SELECT * FROM teacher_grades
-WHERE grade_profesor = 100;
-
 SELECT COUNT(DISTINCT teacherID) FROM teacher_grades
-WHERE grade_profesor = 100;
+WHERE grade_profesor = 100; # Solo 22 profesores
 
-SELECT COUNT(*) FROM users
-WHERE semester = '3';
-
-# SELECT teacherID IF 'FÍSICA' IN ONE OF THE SUBJECT FIELDS
+# SELECT teacherID IF 'FIS' IN ONE OF THE SUBJECT FIELDS
+# Saber los profesores de cierta asignatura
 SELECT teacherID FROM teachers
-WHERE  Subject1 = 'FÍSICA' OR
-       Subject2 = 'FÍSICA' OR
-         Subject3 = 'FÍSICA';
+WHERE  Subject1 = 'FIS' OR
+       Subject2 = 'FIS' OR
+         Subject3 = 'FIS';
 
-SELECT COUNT(*) as NumProfesores FROM teachers
-WHERE  Subject1 = 'FÍSICA' OR
-       Subject2 = 'FÍSICA' OR
-         Subject3 = 'FÍSICA'; # 3
+# Saber cuantos profesores de cierta asignatura
+SELECT COUNT(teacherID) FROM teachers
+WHERE  Subject1 = 'FIS' OR
+       Subject2 = 'FIS' OR
+         Subject3 = 'FIS';
