be1db2b79b475f199ade071bc206c900a80e8540
eUlisesBaezGt <89673617+eUlisesBaezGt@users.noreply.github.com>
2023-05-15 23:26:42 -0600

    ya quedó el dropdown de los profes y ya se actualiza

--- test_front/professors.php
+++ test_front/professors.php
@@ -21,6 +21,7 @@ if ($result->num_rows > 0) {
     while($row = $result->fetch_assoc()) {
         $teacherID[] = $row["teacherID"];
     }
+    echo json_encode($teacherID);
 } else {
     echo "0 results";
 }
