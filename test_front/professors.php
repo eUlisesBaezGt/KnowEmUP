dba12d7fa80e0daa4d9ca2f38ccae51ad189dc68
eUlisesBaezGt <89673617+eUlisesBaezGt@users.noreply.github.com>
2023-05-15 22:55:58 -0600

    Professors PHP completado

---
+++ test_front/professors.php
@@ -0,0 +1,30 @@
+<?php
+$servername = "localhost:3306";
+$username = "root";
+$password = "root";
+$dbname = "KnowEmUP";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "SELECT teacherID FROM teachers";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    // output data of each row
+    $teacherID = array();
+    while($row = $result->fetch_assoc()) {
+        $teacherID[] = $row["teacherID"];
+    }
+    echo json_encode($teacherID);
+} else {
+    echo "0 results";
+}
+
+$conn->close();
+?>
