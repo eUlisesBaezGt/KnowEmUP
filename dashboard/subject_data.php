ef162d046f72898d84e81ccb58b925414a584d16
eUlisesBaezGt <89673617+eUlisesBaezGt@users.noreply.github.com>
2023-05-16 15:35:15 -0600

    intento de barra 2

---
+++ dashboard/subject_data.php
@@ -0,0 +1,38 @@
+<?php
+$selectedSubject = $_GET['subject'];
+
+$servername = "localhost:3306";
+$username = "root";
+$password = "root";
+$dbname = "KnowEmUP";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "SELECT t.`teacherID`, AVG(g.grade_profesor) as avg_grade 
+        FROM teachers t 
+        JOIN teacher_grades g ON t.`teacherID` = g.teacherID 
+        WHERE t.Subject1 = '$selectedSubject' OR t.Subject2 = '$selectedSubject' OR t.Subject3 = '$selectedSubject' 
+        GROUP BY t.`teacherID`";
+$result = $conn->query($sql);
+
+$data = array();
+
+if ($result->num_rows > 0) {
+    while ($row = $result->fetch_assoc()) {
+        $data[] = array(
+            'teacher' => $row['teacherID'],
+            'avg_grade' => $row['avg_grade']
+        );
+    }
+}
+
+echo json_encode($data);
+
+$conn->close();
+?>
---
+++ dashboard/subjects.php
@@ -0,0 +1,29 @@
+<?php
+$servername = "localhost:3306";
+$username = "root";
+$password = "root";
+$dbname = "KnowEmUP";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "SELECT DISTINCT Subject1 AS Subject FROM teachers WHERE Subject1 IS NOT NULL AND Subject1 != '' UNION SELECT DISTINCT Subject2 FROM teachers WHERE Subject2 IS NOT NULL AND Subject2 != '' UNION SELECT DISTINCT Subject3 FROM teachers WHERE Subject3 IS NOT NULL AND Subject3 != ''";
+$result = $conn->query($sql);
+
+$subjects = array();
+
+if ($result->num_rows > 0) {
+    while ($row = $result->fetch_assoc()) {
+        $subjects[] = $row['Subject'];
+    }
+}
+
+echo json_encode($subjects);
+
+$conn->close();
+?>
